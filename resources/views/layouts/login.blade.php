6a41162b5a0e591c4186e237a4251167a15e3f8d
elatfimohamed <mohamedelatfi93@gmial.com>
2019-01-08 19:57:52 +0100

    manifest

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="manifest" href="/manifest.json">
+
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="manifest" href="/manifest.json">
     <title>@yield('title')</title>
     <style>
         [v-cloak] { display: none }
